d63d2204fec33c11e8b6bf34279d7cdd5f9520cf
singshivendra19809@gmail.com <singshivendra19809@gmail.com>
2025-07-22 22:22:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  )
+}
+
+export default App
